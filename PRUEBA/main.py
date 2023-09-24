1c0af8e0f9b2ca4cfef478b8ee4136f87c52f086
Juleipssy <juleipssy.cely@correounivalle.edu.co>
2023-09-24 15:06:05 -0500

    ventana main creada

---
+++ PRUEBA/main.py
@@ -0,0 +1,130 @@
+import pygame, sys, random
+from button import Button
+#from window import window
+
+
+#color 
+red = (255,0,0)
+
+
+pygame.init()
+W,H = 1280, 720
+SCREEN = pygame.display.set_mode((W,H))
+pygame.display.set_caption("Menu")
+
+#music
+pygame.mixer.music.load("Audio/fire.wav")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(2)
+
+
+BG = pygame.image.load("assets/Background.png").convert()
+x=0
+
+
+
+
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+
+
+        PLAY_TEXT = get_font(45).render("window", True, "White")
+        #self.juego_activado = False
+        
+
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                SCREEN.blit(BG,(x,0))
+                x -= 1
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+    
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+      
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+        
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+main_menu()
---
+++ PRUEBA/window.py
@@ -0,0 +1,85 @@
+import pygame
+
+pygame.init()
+
+INIT_STATE = [
+[0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+[0, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+[0, 1, 0, 2, 0, 0, 0, 0, 0, 1],
+[0, 1, 0, 1, 1, 1, 1, 1, 0, 0],
+[5, 0, 0, 6, 4, 0, 0, 1, 0, 1],
+[0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+[3, 0, 0, 0, 2, 0, 0, 1, 0, 1],
+[0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+[0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+[0, 1, 0, 1, 1, 1, 0, 0, 0, 0]
+]
+
+#colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255,0,0)
+GREEN = (0,153,0)
+BLUE = (0,102,255)
+GREY = (96,96,96)
+ORANGE = (250,145,54)
+
+font = pygame.font.Font(None, 32)
+
+weight = 600
+height = 600
+screen = pygame.display.set_mode((weight, height))
+
+
+def draw_grid():
+    # spacing between each line
+    spacing = 60
+
+    # draw horizontal lines
+    for i in range(0, height, spacing):
+        pygame.draw.line(screen, BLACK, (0, i), (weight, i))
+
+    # draw vertical lines
+    for i in range(0, weight, spacing):
+        pygame.draw.line(screen, BLACK, (i, 0), (i, height))
+
+def print_rects(state):
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            if state[i][j] == 1:
+                pygame.draw.rect(screen, GREY, (j*60, i*60, 60, 60))
+            elif state[i][j] == 2:
+                pygame.draw.rect(screen, ORANGE, (j*60, i*60, 60, 60))
+            elif state[i][j] == 3:
+                pygame.draw.rect(screen, RED, (j*60, i*60, 60, 60))
+                text = font.render("1L",True,BLACK)
+                screen.blit(text,(j*60+20,i*60+20))
+            elif state[i][j] == 4:
+                pygame.draw.rect(screen, RED, (j*60, i*60, 60, 60))
+                text = font.render("2L",True,BLACK)
+                screen.blit(text,(j*60+20,i*60+20))
+            elif state[i][j] == 5:
+                pygame.draw.rect(screen, GREEN, (j*60, i*60, 60, 60))
+            elif state[i][j] == 6:
+                pygame.draw.rect(screen, BLUE, (j*60, i*60, 60, 60))
+            elif state[i][j] == 7:
+                pygame.draw.rect(screen, WHITE, (j*60, i*60, 60, 60))
+            else:
+                pygame.draw.rect(screen, WHITE, (j*60, i*60, 60, 60))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.fill(WHITE)
+
+    #---draw here---
+    print_rects(INIT_STATE)
+    draw_grid()
+    
+
+    #---end draw---
+    pygame.display.flip()
